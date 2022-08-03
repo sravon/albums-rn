d377ec2aae5f42f71f684d61c8e818561ae16ebd
md sravon <kaziar42@gmail.com>
2022-08-03 18:51:29 +0600

    first react app

---
+++ index.js
@@ -0,0 +1,8 @@
+// Index.ios.js - place code here for IOS
+
+// Import a library to helpcreate a component.
+import React from 'react';
+import ReactNative from 'react-native';
+// Create a comment
+
+// render it to device
